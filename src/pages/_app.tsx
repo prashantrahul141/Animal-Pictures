ecfab8f7bf3d9c1c968cd3700709601416b7a425
Prashant <prashantrahul141@protonmail.com>
2023-05-13 16:11:41 +0530

    add base bg to page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='fixed left-0 top-0 h-screen w-screen bg-bg'>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
