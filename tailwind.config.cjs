7825c89a875e9c1f97222d896a0d2954e846a5cb
Prashant <prashantrahul141@protonmail.com>
2023-05-13 14:35:54 +0530

    add bg color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,12 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        bg: '#1e1e1e',
+      },
+    },
+  },
   plugins: [],
 };
