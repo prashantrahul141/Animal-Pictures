b011cd0657807624ae9b43653a9431ae97a8ece1
Prashant <prashantrahul141@protonmail.com>
2023-05-13 16:11:55 +0530

    index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <div className='h-screen w-screen'>
+      <div className='my-16 flex justify-center'>
+        <span className='font-sans text-4xl font-extrabold text-white md:text-5xl '>
+          Animal Pictures
+        </span>
+      </div>
+      <div className='flex flex-col items-center justify-center gap-6'>
+        <Link href={'/cat'}>
+          <button className='btn'>Cats</button>
+        </Link>
+        <Link href={'/dog'}>
+          <button className='btn'>Dogs</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
