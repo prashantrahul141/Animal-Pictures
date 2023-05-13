0e518d642349c1a790f776906e1828ee200a0125
Prashant <prashantrahul141@protonmail.com>
2023-05-13 13:37:21 +0530

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("next").NextConfig} */
+const config = {
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'export',
+};
+
+export default config;
