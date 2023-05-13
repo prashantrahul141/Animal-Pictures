9af802a20af714c89e973a20f1bb5730994bbac3
Prashant <prashantrahul141@protonmail.com>
2023-05-13 16:12:25 +0530

    dog page file and components

---
+++ src/pages/dog.tsx
@@ -0,0 +1,56 @@
+import type { NextPage } from 'next';
+
+import { dogBreedsData } from '@/utils/constants';
+import { useEffect, useState } from 'react';
+
+const Dog: NextPage = () => {
+  const [selectedBreedUrl, setSelectedBreedUrl] = useState(
+    dogBreedsData[0].url
+  );
+  const [dogImageUrl, setDogImageUrl] = useState(dogBreedsData[0].url);
+
+  const fetchData = async () => {
+    const response = await fetch(selectedBreedUrl, { method: 'GET' });
+    const newData: { message: string; status: string } =
+      (await response.json()) as { message: string; status: string };
+    setDogImageUrl(newData.message);
+  };
+
+  useEffect(() => {
+    void fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedBreedUrl]);
+
+  return (
+    <>
+      <div className='h-screen w-screen'>
+        <div className='flex w-full items-center justify-center gap-4 py-12'>
+          <select
+            onChange={(e) => {
+              setSelectedBreedUrl(e.currentTarget.value);
+            }}
+            className='flex w-fit items-center justify-center rounded-md border bg-bg-100 py-1 text-2xl text-white'>
+            {dogBreedsData.map((dogBreed) => {
+              return (
+                <option
+                  className='text-base'
+                  value={dogBreed.url}
+                  key={dogBreed.id}>
+                  {dogBreed.name}
+                </option>
+              );
+            })}
+          </select>
+          <button onClick={fetchData} className='btn w-12'>
+            Next
+          </button>
+        </div>
+        <div className='w-ful flex items-center justify-center p-12'>
+          <img src={dogImageUrl} alt='dog' />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dog;
